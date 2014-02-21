0758b2ca24b438a0abfd820af3b3de2de407c787
SudoAttack <wayne8798@gmail.com>
2014-02-21 14:35:07 -0600

    now we can publish hit

--- backend/mturk_manager.py
+++ backend/mturk_manager.py
@@ -2,64 +2,41 @@ import datetime
 import boto.mturk.connection
 import boto.mturk.question as mtq
 import boto.mturk.qualification as mtqu
-mt = boto.mturk.connection.MTurkConnection(aws_access_key_id='AKIAIMKMN5ICEWOJFFAA',
-                                           aws_secret_access_key='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')
 
-text_en = '''Hello!
-I am test text message to be translated from English to Russian.
-If you ask me, I was born in a mind of a crazy web developer,
-who tests the MTurk API to start a very promising service later.
-'''
+AWS_ACCESS_KEY_ID = 'AKIAIMKMN5ICEWOJFFAA'
+AWS_SECRET_ACCESS_KEY = '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'
+HOST = 'mechanicalturk.sandbox.amazonaws.com'
+mt = boto.mturk.connection.MTurkConnection(AWS_ACCESS_KEY_ID,
+                                           AWS_SECRET_ACCESS_KEY,
+                                           host=HOST)
 
-question_id = datetime.datetime.utcnow().strftime("%Y-%m-%d-%H-%M-%S")
-print('id=%s' % question_id)
- 
-content1 = mtq.QuestionContent()
-content1.append_field('Title', 'Translate a text between the markers below from English to Russian.')
-content1.append_field('Title', 'Human translation only! Machine tranlations will be rejected.')
-content1.append_field('Text', '================= FROM HERE =================')
-content1.append_field('Text', text_en)
-content1.append_field('Text', '================= TILL HERE =================')
- 
-content2 = mtq.QuestionContent()
-content2.append_field('Title', 'Any notes? Advices? Emotions? (Optional)')
- 
-question1 = mtq.Question(
-	question_id,
-	content = content1,
-	answer_spec = mtq.AnswerSpecification(mtq.FreeTextAnswer()),
-	is_required = True,
-	)
-question2 = mtq.Question(
-	question_id+'-b',
-	content = content2,
-	answer_spec = mtq.AnswerSpecification(mtq.FreeTextAnswer()),
-	is_required = False,
-	)
+content = mtq.QuestionContent()
+content.append_field('Text', 'Please tell us your first impression about the picture:')
+image_link = '<img src="http://i.imgur.com/ZiVaPei.jpg" alt="target"></img>'
+content.append(mtq.FormattedContent(image_link))
+
+question = mtq.Question(
+    'response',
+    content = content,
+    answer_spec = mtq.AnswerSpecification(mtq.FreeTextAnswer()),
+    is_required = True,
+    )
  
 qualifications = mtqu.Qualifications()
 qualifications.add(mtqu.PercentAssignmentsApprovedRequirement('GreaterThanOrEqualTo', 75))
- 
+
 res = mt.create_hit(
-	questions=[question1, question2],
-	qualifications=qualifications,
- 
-	title = 'Translate 3 lines from English to Russian (human translation needed).',
-	description = 'Translate 3 lines of English to Russian. No machine translations will be accepted. Only human translation.',
-	keywords='translate, translation, english, russian',
-	
-	# These things affect the total cost:
-	reward=mt.get_price_as_price(0.05),
-	max_assignments=2,
-	
-	# These are for scheduling and timing out.
-	approval_delay=datetime.timedelta(seconds=24*60*60),# auto-approve timeout
-	duration=datetime.timedelta(seconds=15*60),# how fast the task is abandoned if not finished
-	)
- 
+    questions=[question],
+    qualifications=qualifications,
+    title = 'Image Impression Survey',
+    description = 'Tell us your thought about the provided image',
+    keywords='image, impression',
+    reward=mt.get_price_as_price(0.05),
+    max_assignments=1,
+    approval_delay=datetime.timedelta(seconds=24*60*60),# auto-approve timeout
+    duration=datetime.timedelta(seconds=15*60),# how fast the task is abandoned if not finished
+    )
+
 hit = res[0]
 hit_id = hit.HITId
 print('hit id = %s' % hit_id)
-filename = 'hit-%s.txt' % question_id
-file(filename, 'wt').write(hit_id)
-print('saved to %s' % filename)
